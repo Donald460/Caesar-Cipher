308221ac02a8a7b9cb4e78d333edeb0ed6be65b4
Donald460 <126630500+Donald460@users.noreply.github.com>
2023-12-10 13:50:56 -0500

    Update mycaesar.py

--- mycaesar.py
+++ mycaesar.py
@@ -1,6 +1,7 @@
 #Importing the logo from art.py
 from art import logo
 print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
